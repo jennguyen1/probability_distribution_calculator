01a3013eb6b26df3bc78a08091c6c3429f616707
Jenny <Nhuy@Jennifers-MacBook-Air.local>
2016-06-28 19:32:01 -0500

    upgrade UI code

--- ui.R
+++ ui.R
@@ -7,35 +7,35 @@ library(shinydashboard)
 
 # ui function
 dashboardPage(
-  
+
   # App Title
   dashboardHeader(title = "Probability Distribution Calculator"),
-  
+
   # Disable Sidebar
   dashboardSidebar(disable = TRUE),
-  
+
   # Vertical layout for inputs
   dashboardBody( fluidRow(
     ######################################
     # Select Distribution and Parameters #
     ######################################
     box(
-      
+
       # set box title and image of the box
-      title = "Select Distribution", 
+      title = "Select Distribution",
       status = "primary", solidHeader = TRUE, collapsible = TRUE,
 
       # box contents
       wellPanel(
-        
+
         # print X distribution in common notation
         h3(textOutput("model_descr"), align = "center"),
         br(),
-        
+
         tabsetPanel(
-          
+
           # CONTINUOUS DISTRIBUTIONS #
-          
+
           # Normal distribution
           tabPanel(
             "Normal",
@@ -86,9 +86,9 @@ dashboardPage(
               numericInput("beta.shape2", "shape 2", 1)
             )
           ),
-          
+
           # DISCRETE DISTRIBUTIONS #
-          
+
           # Binomial distribution
           tabPanel(
             "Binomial",
@@ -110,22 +110,22 @@ dashboardPage(
               numericInput("nbinom.p", "probability of success", 0.5)
             )
           ),
-          
+
           # panel descriptions
           id = "dist"
         )
       )
     ),
-    
+
     ##################
     # Select Options #
     ##################
     box(
-      
+
       # set box title and image of the box
-      title = "Select Options", 
-      solidHeader = TRUE, status = "primary", collapsible = TRUE, 
-      
+      title = "Select Options",
+      solidHeader = TRUE, status = "primary", collapsible = TRUE,
+
       # box contents
       wellPanel( verticalLayout(
 
@@ -137,41 +137,41 @@ dashboardPage(
           ),
           h3(textOutput("prob_descr"), align = "center")
         ),
-        
-        br(), 
-        
+
+        br(),
+
         # input percentiles or quantiles
         flowLayout(
-          
+
           # option to select x or p
           radioButtons("type", "input", c("x", "probability")),
-          
+
           # select X
           verticalLayout(
           uiOutput("x_option1"),
           uiOutput("x_option2"),
-          
+
           # select p
           uiOutput("p_option"))
         )
       ))
     )),
-    
+
     ################
     # Plot Outputs #
     ################
-    fluidRow(column(width = 12, 
+    fluidRow(column(width = 12,
     box(width = NULL,
-      #mainPanel(
+
       title = "Plots",
-      solidHeader = TRUE, status = "primary", 
-      
+      solidHeader = TRUE, status = "primary",
+
       # statement of distribution and model
       h3(textOutput("prob"), align = "center"),
-      
+
       # density function plot with shaded regions
       plotOutput("density", height = "300px"),
-      
+
       # cumulative density function plot if P(X < x)
       plotOutput("cdf", height = "300px")
     )
